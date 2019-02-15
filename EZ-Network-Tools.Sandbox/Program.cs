14de53fd4f705e8a5959510f1a134662def5ae2b
Christian Campana-Emard <christian.campana.emard@outlook.com>
2019-02-15 17:43:35 -0500

    prints and numbers network adapters. Filters for active adapters only

--- EZ-Network-Tools.Sandbox/Program.cs
+++ EZ-Network-Tools.Sandbox/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
 
 namespace EZ_Network_Tools.Sandbox
 {
@@ -6,7 +9,62 @@ namespace EZ_Network_Tools.Sandbox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Process p = new Process();
+            // ProcessStartInfo psi = new ProcessStartInfo("netsh", "interface ip set address \"Local Area Connection\" static 192.168.0.10 255.255.255.0 192.168.0.1 1");
+            // p.StartInfo = psi;
+            // p.Start();
+
+            // get ALL network adapters and print
+            Console.WriteLine("<----All Adapters---->");
+            var allAdapters = NetworkInterface.GetAllNetworkInterfaces();
+            PrintDNSAddresses(allAdapters);
+
+            // get ONLY active network interfaces and print
+            Console.WriteLine("<----Active Adapters---->");
+            List<NetworkInterface> activeAdapters = (List<NetworkInterface>)GetActiveNetworkInterfaces();
+            PrintDNSAddresses(activeAdapters);
+        }
+
+        /// <summary>
+        /// Gets network adapters that are up and aren't loopback
+        /// </summary>
+        /// <returns>IEnumerable of NetworkInterfaces</returns>
+        public static IEnumerable<NetworkInterface> GetActiveNetworkInterfaces()
+        {
+            NetworkInterface[] allAdapters = NetworkInterface.GetAllNetworkInterfaces();
+
+            List<NetworkInterface> activeAdapters = new List<NetworkInterface>();
+
+            foreach (NetworkInterface adapter in allAdapters)
+            {
+                if(adapter.OperationalStatus.HasFlag(OperationalStatus.Up) &&
+                    !adapter.Name.ToLower().Contains("loopback") &&
+                    !adapter.Name.ToLower().Contains("pseudo"))
+                {
+                    activeAdapters.Add(adapter);
+                }
+            }
+
+            return activeAdapters;
+        }
+
+        /// <summary>
+        /// Given a list of NetworkInterfaces, then print the DNS addresses to console
+        /// </summary>
+        /// <param name="networkInterfaces"></param>
+        public static void PrintDNSAddresses(IList<NetworkInterface> networkInterfaces)
+        {
+            for(int i = 0; i < networkInterfaces.Count; i++)
+            {
+                IPInterfaceProperties properties = networkInterfaces[i].GetIPProperties();
+                Console.WriteLine($"#{i+1}: Adapter name: {networkInterfaces[i].Name}");
+                Console.WriteLine($"DNS Addresses:");
+                foreach(var address in properties.DnsAddresses)
+                {
+                    Console.WriteLine(address.ToString());
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
