9b9053dd93d1de6a83aa064fdfbd92b7399dcfe0
Christian Campana-Emard <christian.campana.emard@outlook.com>
2019-02-15 14:21:09 -0500

    made blank .NET Core sandbox

---
+++ EZ-Network-Tools.Sandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EZ_Network_Tools.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
